d752812d8945f3d88b9b86474019c6cee22ff52a
omniofficial <omniofficial04@gmail.com>
2025-08-31 13:10:47 -0700

    String interpolation

--- js_Foundations/arraymethods/external_looptest.js
+++ js_Foundations/arraymethods/external_looptest.js
@@ -1,5 +1,17 @@
-const dogs = ["Doggo1", "Doggo2", "Doggo3", "Doggo4", "Doggo5"];
+const cats = ["Pete", "Biggles", "Jasmine"];
 
-for (let i = 0; i < dogs.length; i++) {
-    console.log(dogs[i]);
+let myFavoriteCats = "My cats are called ";
+
+let i = 0;
+
+while (i < cats.length) {
+    if (i === cats.length - 1) {
+        myFavoriteCats += `and ${cats[i]}.`;
+    } else {
+        myFavoriteCats += `${cats[i]}, `;
+    }
+
+    i++;
 }
+
+console.log(myFavoriteCats); // "My cats are called Pete, Biggles, and Jasmine."
