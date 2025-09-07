5efd85fc230cb63251f57dbcfe0f804697c61585
omniofficial <omniofficial04@gmail.com>
2025-09-07 14:14:40 -0700

    Added test node

--- js_Foundations/mdndommanipulation/script.js
+++ js_Foundations/mdndommanipulation/script.js
@@ -1,9 +1,19 @@
+// Adding link to clickable link
 const link = document.querySelector("a");
 link.textContent = "Mozilla Developer Network";
 link.href = "https://developer.mozilla.org";
 
+// Adding a paragraph section and appending it to the DOM
 const sect = document.querySelector("section");
 const para = document.createElement("p");
 para.textContent = "We hope you enjoyed the ride.";
 sect.appendChild(para);
+
+// Adding a text node
+const text = document.createTextNode(
+    " - the premier source for web development knowledge."
+);
+
+const linkPara = document.querySelector("a");
+linkPara.appendChild(text);
 
