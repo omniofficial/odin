69757e4e00714dcfdd3320f8afb65cd149da1515
omniofficial <omniofficial04@gmail.com>
2025-09-07 14:11:25 -0700

    Attributes added

---
+++ js_Foundations/mdndommanipulation/script.js
@@ -0,0 +1,9 @@
+const link = document.querySelector("a");
+link.textContent = "Mozilla Developer Network";
+link.href = "https://developer.mozilla.org";
+
+const sect = document.querySelector("section");
+const para = document.createElement("p");
+para.textContent = "We hope you enjoyed the ride.";
+sect.appendChild(para);
+
