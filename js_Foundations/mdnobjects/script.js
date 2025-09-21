a4f7c4a4c654cbf15a6ff474a76e70cba0a5aa1f
omniofficial <omniofficial04@gmail.com>
2025-09-21 15:18:55 -0700

    Object review

---
+++ js_Foundations/mdnobjects/script.js
@@ -0,0 +1,36 @@
+const person = {
+    name: {
+        first: "Bob",
+        last: "Smith",
+    },
+    age: 32,
+    bio() {
+        console.log(
+            `${this.name[0]} ${this.name[1]} is ${this.age} years old.`
+        );
+    },
+    introduceSelf() {
+        console.log(`Hi! I'm ${this.name[0]}.`);
+    },
+};
+
+const myDataName = "height";
+const myDataValue = "1.75m";
+person[myDataName] = myDataValue;
+
+// ------------------------------------------------------------
+
+function Person(name) {
+    this.name = name;
+    this.introduceSelf = function () {
+        console.log(`Hi! I'm ${this.name}.`);
+    };
+}
+
+const salva = new Person("Salva");
+salva.introduceSelf();
+// "Hi! I'm Salva."
+
+const frankie = new Person("Frankie");
+frankie.introduceSelf();
+// "Hi! I'm Frankie."
