7d5d623e90520f8802ff4e7acd48764bfccd65ad
omniofficial <omniofficial04@gmail.com>
2026-01-19 16:42:19 -0800

    E56 Import / Exports

---
+++ javascript/exports/one.js
@@ -0,0 +1,4 @@
+// one.js
+const greeting = "Hello, Odinite!";
+const farewell = "Bye bye, Odinite!";
+export { greeting, farewell };
---
+++ javascript/exports/two.js
@@ -0,0 +1,4 @@
+import { greeting, farewell } from "./one.js";
+
+console.log(greeting); // "Hello, Odinite!"
+console.log(farewell); // "Bye bye, Odinite!"
