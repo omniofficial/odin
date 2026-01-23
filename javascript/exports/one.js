b8de985ccc1ea089fe6ac66d1122f5f5fecdf389
omniofficial <omniofficial04@gmail.com>
2026-01-22 17:11:58 -0800

    test

--- javascript/exports/one.js
+++ javascript/exports/one.js
@@ -5,3 +5,5 @@ export { greeting, farewell };
 // OR
 const greeting1 = "Hello, Odinite!";
 export default greeting1;
+
+// For entry points, the importer depends on the exporter. So we only need to src link the two.js.
