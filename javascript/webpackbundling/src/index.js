ddc98cdfc36a1e4976b2ee91778ae79d4716da52
omniofficial <omniofficial04@gmail.com>
2026-01-29 21:25:53 -0800

    Add image selector

--- javascript/webpackbundling/src/index.js
+++ javascript/webpackbundling/src/index.js
@@ -1,4 +1,8 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
-
+import odinImage from "./odin.png";
 console.log(greeting);
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
