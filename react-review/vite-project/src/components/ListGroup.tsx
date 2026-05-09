a7f2ff2888a56f219adbebaa0697b4eff6118cf2
omniofficial <omniofficial04@gmail.com>
2026-05-08 20:37:54 -0700

    Ternary operators

---
+++ react-review/vite-project/src/components/ListGroup.tsx
@@ -0,0 +1,18 @@
+function ListGroup() {
+    const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+    return (
+        <>
+            <h1>List</h1>
+            {items.length === 0 ? <p>No items found</p> : null}
+            {items.length === 0 && <p>No items found</p>}
+            <ul className="list-group">
+                {items.map((item) => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
