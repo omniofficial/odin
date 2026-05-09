043bed26af0d9bfd69fb216ede196949c5dbc014
omniofficial <omniofficial04@gmail.com>
2026-05-08 23:25:30 -0700

    Props

--- react-review/vite-project/src/App.tsx
+++ react-review/vite-project/src/App.tsx
@@ -1,13 +1,12 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+    const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
     return (
         <div>
-            <ListGroup></ListGroup>
+            <ListGroup items={items} heading="Cities"></ListGroup>
         </div>
     );
 }
 
 export default App;
-
-
