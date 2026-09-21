bcaed132842c0b67cc725644b279e9e130a0856b
omniofficial <omniofficial04@gmail.com>
2026-09-21 16:39:23 -0700

    Feature 1 done

--- javascript/gipy-api/script.js
+++ javascript/gipy-api/script.js
@@ -1,12 +1,17 @@
-const img = document.querySelector("img");
+// Feature 1 Logic
+const randomBtn = document.querySelector("#image-btn");
+randomBtn.addEventListener("click", (e) => {
+    const img = document.querySelector("#random-img");
+    fetch(
+        "https://api.giphy.com/v1/gifs/random?api_key=EkJSRJwEow7YlX30KMffsIXz7xYCGMS7",
+    )
+        .then(function (response) {
+            return response.json();
+        })
 
-fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=EkJSRJwEow7YlX30KMffsIXz7xYCGMS7&s=cats&rating=g",
-)
-    .then(function (response) {
-        return response.json();
-    })
+        .then(function (response) {
+            img.src = response.data.images.original.url;
+        });
+});
 
-    .then(function (response) {
-        img.src = response.data.images.original.url;
-    });
+// Feature 2 Logic
